d4c039b0d16f37f3ee9a27bd437e5eb361a9b6ce
Dale Ma <dalema22@gmail.com>
2015-04-27 01:48:41 -0600

    set up main styles

--- Brocfile.js
+++ Brocfile.js
@@ -1,12 +1,18 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
 
 var app = new EmberApp();
 
 app.import("bower_components/typeahead.js/dist/typeahead.jquery.min.js");
 app.import("bower_components/typeahead.js/dist/bloodhound.min.js");
 app.import("bower_components/jquery-ui/jquery-ui.min.js");
+
+app.import("bower_components/jquery-ui/themes/redmond/jquery-ui.css");
+app.import("bower_components/weather-icons/css/weather-icons.css");
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
@@ -20,4 +26,13 @@ app.import("bower_components/jquery-ui/jquery-ui.min.js");
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+// module.exports = app.toTree();
+// Copy only the relevant files:
+var fontOpenSans = pickFiles('bower_components/weather-icons/font', {
+   srcDir: '/',
+   files: ['**/*.eot', '**/*.svg', '**/*.woff', '**/*.ttf', '**/*.otf'],
+   destDir: '/font'
+});
+
+// Merge the app tree and our new font assets.
+module.exports = mergeTrees([app.toTree(), fontOpenSans]);
--- app/controllers/dayitem.js
+++ app/controllers/dayitem.js
@@ -12,6 +12,45 @@ export default Ember.Controller.extend({
     return Math.ceil(intensity * 10000 / 2).toFixed(2) % 100;
   }.property('model.daily.data'),
 
+  icon: function() {
+    var icon = 'wi-day-sunny'
+    switch(this.get('model.daily.data')[0].icon) {
+      case 'clear-day':
+        icon = 'wi-day-sunny';
+        break;
+      case 'clear-night':
+        icon = 'wi-night-clear';
+        break;
+      case 'rain':
+        icon = 'wi-rain';
+        break;
+      case 'snow':
+        icon = 'wi-snow';
+        break;
+      case 'sleet':
+        icon = 'wi-sleet';
+        break;
+      case 'wind':
+        icon = 'wi-day-windy';
+        break;
+      case 'fog':
+        icon = 'wi-day-fog';
+        break;
+      case 'cloudy':
+        icon = 'wi-day-cloudy';
+        break;
+      case 'partly-cloudy-day':
+        icon = 'wi-day-cloudy';
+        break;
+      case 'partly-cloudy-night':
+        icon = 'wi-night-cloudy';
+        break;
+    }
+
+    return 'wi ' + icon;
+
+  }.property('model.daily.data'),
+
   rainfallStyle: function() {
     return ('height:' + this.get('rainfallPercent') + '%').htmlSafe();
   }.property('rainfallPercent')
